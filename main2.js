4eb645edc515deec5893a9a4e5150fcefc7706c8
Dario Sommer <dariosommer@hotmail.com>
2019-08-07 01:51:56 +0200

    added small design changes

--- main2.js
+++ main2.js
@@ -1,21 +1,29 @@
 var opacityLogo = 1;
 var opacityQuote = 0;
+var smartphone = false;
 
 function fadeLogoIn() {
+    if(smartphone){
+        document.getElementById('name').style.display = 'block';
+    }
+    document.getElementById('logo').style.display = 'block';
     if (opacityLogo<1) {
         opacityLogo += .075;
         setTimeout(function(){fadeLogoIn()},100);
     }
     document.getElementById('logo').style.opacity = opacityLogo;
+    if(smartphone){
+        document.getElementById('name').style.opacity = opacityLogo;
+    }
 }
 
 function fadeQuoteIn() {
+    document.getElementById('quoteContainer').style.display = 'block';
     if (opacityQuote<1) {
         opacityQuote += .075;
         setTimeout(function(){fadeQuoteIn()},100);
     }
     document.getElementById('quoteContainer').style.opacity = opacityQuote;
-    document.getElementById('cite').style.opacity = opacityQuote;
 }
 
 function fadeLogoOut(){
@@ -24,6 +32,16 @@ function fadeLogoOut(){
         setTimeout(function (){fadeLogoOut()}, 100);
     }
     document.getElementById('logo').style.opacity = opacityLogo;
+    if(smartphone){
+        document.getElementById('name').style.opacity = opacityLogo;
+    }
+    if (opacityLogo<=0){
+        document.getElementById('logo').style.display = 'none';
+        if(smartphone){
+            document.getElementById('name').style.display = 'none';
+        }
+        fadeQuoteIn();
+    }
 }
 
 function fadeQuoteOut(){
@@ -32,13 +50,20 @@ function fadeQuoteOut(){
         setTimeout(function (){fadeQuoteOut()}, 100);
     }
     document.getElementById('quoteContainer').style.opacity = opacityQuote;
-    document.getElementById('cite').style.opacity = opacityQuote;
+    if (opacityQuote<=0){
+        document.getElementById('quoteContainer').style.display = 'none';
+        fadeLogoIn();
+    }
 }
     // start a typewriter animation for a text in the dataText array
     function StartTextAnimation() {
+        if(window.innerWidth<800){
+            smartphone = true;
+        } else {
+            smartphone = false;
+        }
         fadeLogoOut();
-        fadeQuoteIn();
         setTimeout(function() {
-            fadeQuoteOut(); fadeLogoIn();
-        }, 5000);
+            fadeQuoteOut();
+        }, 7500);
     }
